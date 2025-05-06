ada1a68498a5f384054a973a2f3d12fa41b96cbf
80HD Studios <afunnierguy@gmail.com>
2025-05-05 18:49:10 -0700

    Remove the uninstaller (which is system-tailored)
    the uninstaller is system-tailored and is not universal. It's generated on install. I accidentally uploaded it...

--- GoldenLauncherUninstaller.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Uninstalling GoldenLauncher"
-rm -rf "/home/mel/.local/bin/GoldenLauncher"
-rm -f "/home/mel/.local/share/applications/GoldenLauncher.desktop"
-rm -f GoldenLauncherUninstaller.sh
-rm -f "/home/mel/.local/bin/goldenlauncher"
-echo "Successfully Removed!"
